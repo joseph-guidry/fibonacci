326b99b6a06ee5b7f3c5da08b12847e7a78054d4
joseph.guidry <joseph.guidry4@gmail.com>
2017-11-09 15:21:11 -0500

    Remove extra print statement

--- fibonacci.s
+++ fibonacci.s
@@ -42,105 +42,94 @@ main:
 	mov		r15, rsi
 
 	cmp 	edi, 2				; Check if the CL args == 1  (jump on equal)
-	jne 		help
-
-	;cmp 	edi, 2
-	;jne 	Usage_Error			;If more than 1 CL arguement => ERROR ( jump on not equal )
-		
-	;Print what is entered on the command line if arguments are successful-> could remove later.
-	mov 	rdi, Echo
-	mov 	rsi, [r15]
-	mov 	rdx, [r15 + 8]
-	xor 	rax, rax
-	call 	printf
+	jne 	help
 
 	; Convert arguement to input value
-	mov rdi, [r15 + 8]
-	mov rdx, 10
-	lea rsi, [rsp]
-	call strtoul
-	mov rcx, [rsp]
-	mov qword[temp], rax
+	mov 	rdi, [r15 + 8]
+	mov 	rdx, 10
+	lea 	rsi, [rsp]
+	call	strtoul
+	mov 	rcx, [rsp]
+	mov 	qword[temp], rax
 	
 	;Check to see if the user entered the value 0 on the command line
-	cmp qword[temp], 0
-	je 	print_output;
+	cmp 	qword[temp], 0
+	je 		print_output;
 
 	;set the input value from the command line as the counter, which will decrement to one.
-	mov rbx, qword[temp]
+	mov 	rbx, qword[temp]
 
 fib_loop:
-	;mov rdi, msg1
-	;mov rsi, rbx
-	;call printf
+	;mov 	rdi, msg1
+	;mov 	rsi, rbx
+	;call 	printf
 
 	; Move the value of a = b
-	mov rdx, qword [b]
-	mov qword [a], rdx
-	mov rdx, qword[b + 8]
-	mov qword[a + 8], rdx
-	mov rdx, qword[b + 16]
-	mov qword[a + 16], rdx
-	mov rdx, qword[b + 24]
-	mov qword[a + 24], rdx
-	mov rdx, qword[b + 32]
-	mov qword[a + 32], rdx
-	mov rdx, qword[b + 40]
-	mov qword[a + 40], rdx
+	mov 	rdx, qword [b]
+	mov 	qword [a], rdx
+	mov 	rdx, qword[b + 8]
+	mov 	qword[a + 8], rdx
+	mov 	rdx, qword[b + 16]
+	mov 	qword[a + 16], rdx
+	mov 	rdx, qword[b + 24]
+	mov 	qword[a + 24], rdx
+	mov 	rdx, qword[b + 32]
+	mov 	qword[a + 32], rdx
+	mov 	rdx, qword[b + 40]
+	mov 	qword[a + 40], rdx
 
 	; Move the value b = total
-	mov rax, qword[total]
-	mov qword[b], rax
-	mov rax, qword[total + 8]
-	mov qword[b + 8], rax
-	mov rax, qword[total + 16]
-	mov qword[b + 16], rax
-	mov rax, qword[total + 24]
-	mov qword[b + 24], rax
-	mov rax, qword[total + 32]
-	mov qword[b + 32], rax
-	mov rax, qword[total + 40]
-	mov qword[b + 40], rax
+	mov 	rax, qword[total]
+	mov 	qword[b], rax
+	mov 	rax, qword[total + 8]
+	mov 	qword[b + 8], rax
+	mov 	rax, qword[total + 16]
+	mov 	qword[b + 16], rax
+	mov 	rax, qword[total + 24]
+	mov 	qword[b + 24], rax
+	mov 	rax, qword[total + 32]
+	mov 	qword[b + 32], rax
+	mov 	rax, qword[total + 40]
+	mov 	qword[b + 40], rax
 
 	; update the total value = a + b
 	; Add lower order chunk
-	mov rax, qword[a]
-	add rax, qword[b]
-	mov qword[total], rax
-	
+	mov 	rax, qword[a]
+	add 	rax, qword[b]
+	mov 	qword[total], rax
+		
 	; Add together 2nd lowest order chunk and carry value.
-	mov rax, qword[a + 8]
-	adc rax, qword[b + 8]
-	mov qword[total + 8], rax
+	mov 	rax, qword[a + 8]
+	adc 	rax, qword[b + 8]
+	mov 	qword[total + 8], rax
+	
+	; Add together next highest order chunk and carry value
+	mov 	rax, qword[a + 16]
+	adc 	rax, qword[b + 16]
+	mov 	qword[total + 16], rax
+	
+	; Add together next highest order chunk and carry value
+	mov 	rax, qword[a + 24]
+	adc 	rax, qword[b + 24]
+	mov 	qword[total + 24], rax
 
 	; Add together next highest order chunk and carry value
-	mov rax, qword[a + 16]
-	adc rax, qword[b + 16]
-	mov qword[total + 16], rax
-
-	; Add together next highest order chunk and carry value
-	mov rax, qword[a + 24]
-	adc rax, qword[b + 24]
-	mov qword[total + 24], rax
-
-	; Add together next highest order chunk and carry value
-	mov rax, qword[a + 32]
-	adc rax, qword[b + 32]
-	mov qword[total + 32], rax
-
+	mov 	rax, qword[a + 32]
+	adc 	rax, qword[b + 32]
+	mov 	qword[total + 32], rax
+	
 	; Add together highest order chunk and carry value
-	mov rax, qword[a + 40]
-	adc rax, qword[b + 40]
-	mov qword[total + 40], rax
+	mov 	rax, qword[a + 40]
+	adc 	rax, qword[b + 40]
+	mov 	qword[total + 40], rax
 
 	; Check if the counter has reached 1
-	cmp ebx, 1
-	je print_output	
+	cmp 	ebx, 1
+	je 		print_output	
 
 	; Decrement the counter
-	dec ebx
-	jmp fib_loop
-	;jmp Return
+	dec 	ebx
+	jmp 	fib_loop
 
 print_output:	; The values need to be in reverse order!
 	;mov 	rdi, msg2
